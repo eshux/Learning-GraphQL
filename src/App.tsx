b301174616a1ccd8b5d03dc3fddb92808a48b93e
Estere <estere.guza@prime-force.com>
2021-10-20 11:33:51 +0300

    Init project with React and esLint

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import './App.css';
+
+const App:FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>
+        Learning GraphQL
+      </p>
+    </header>
+  </div>
+);
+
+export default App;
